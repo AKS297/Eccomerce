21701edd1709baf3af7680ff3895dd1dee503c65
ak <akimmortal297@gmail.com>
2024-04-29 23:56:07 +0530

    "Code Changes for creating JWT token"

--- ecomerce/src/main/java/com/app/ecomerce/service/JWTauthenticationService.java
+++ ecomerce/src/main/java/com/app/ecomerce/service/JWTauthenticationService.java
@@ -1,8 +1,45 @@
 package com.app.ecomerce.service;
 
+import com.app.ecomerce.model.Customer;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class JWTauthenticationService {
 
+    //algorythm key can be HS256 or RS256
+    @Value("${jwt.algorythm.key}")
+    private String algorythmkey;
+
+    //who is going to issue the token
+    @Value("${jwt.token.issuer}")
+    private String tokenIssuer;
+
+    @Value("${jwt.time.seconds}")
+    private int expiryInSeconds;
+
+    private Algorithm algorithm;
+
+    private static final String USERNAME_KEY = "USERNAME";
+
+    @PostConstruct
+    public void postConstruct(){
+        algorithm = Algorithm.HMAC256(algorythmkey);
+    }
+
+    //Generates Jwt token for the customer
+    public String generateJWTToken(Customer customer){
+        return JWT
+                .create()
+                .withClaim(USERNAME_KEY,customer.getUserName())
+                .withExpiresAt(new Date(System.currentTimeMillis() + (1000 * expiryInSeconds)))
+                .withIssuer(tokenIssuer)
+                .sign(algorithm);
+
+    }
 }
